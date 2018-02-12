52d52a32a8b96854d7db9f631d17036f3c62d8f5
Evolution Tea <Evolution Tea@DESKTOP-43087BR>
2018-02-12 20:10:19 +0700

    add bean paymentMapBean

---
+++ src/main/java/th/co/maximus/bean/PaymentMMapPaymentInvBean.java
@@ -0,0 +1,287 @@
+package th.co.maximus.bean;
+
+import java.sql.Timestamp;
+
+public class PaymentMMapPaymentInvBean {
+	private Long manualId;
+	private Long paymentId;
+	private String invoiceNo;
+	private String receiptNoManual;
+	private Timestamp paidDate;
+	private String brancharea;
+	private String branchCode;
+	private Long paidAmount;
+	private String source;
+	private String clearing;
+	private Long refid;
+	private String accountNo;
+	private String clearingSap;
+
+	private Long paymentInvoiceManualId;
+	private Long beforVat;
+	private Long amount;
+	private int customerName;
+	private String customerAddress;
+	private String customerSegment;
+	private String customerBranch;
+	private String taxNo;
+	private String subNo;
+	private String period;
+	private String serviceType;
+	private String printReceipt;
+	private String remark;
+	private String createBy;
+	private Timestamp createDate;
+	private String updateBy;
+	private Timestamp updateDate;
+	private String recordStatus;
+
+	public Long getManualId() {
+		return manualId;
+	}
+
+	public void setManualId(Long manualId) {
+		this.manualId = manualId;
+	}
+
+	public Long getPaymentId() {
+		return paymentId;
+	}
+
+	public void setPaymentId(Long paymentId) {
+		this.paymentId = paymentId;
+	}
+
+	public String getInvoiceNo() {
+		return invoiceNo;
+	}
+
+	public void setInvoiceNo(String invoiceNo) {
+		this.invoiceNo = invoiceNo;
+	}
+
+	public String getReceiptNoManual() {
+		return receiptNoManual;
+	}
+
+	public void setReceiptNoManual(String receiptNoManual) {
+		this.receiptNoManual = receiptNoManual;
+	}
+
+	public Timestamp getPaidDate() {
+		return paidDate;
+	}
+
+	public void setPaidDate(Timestamp paidDate) {
+		this.paidDate = paidDate;
+	}
+
+	public String getBrancharea() {
+		return brancharea;
+	}
+
+	public void setBrancharea(String brancharea) {
+		this.brancharea = brancharea;
+	}
+
+	public String getBranchCode() {
+		return branchCode;
+	}
+
+	public void setBranchCode(String branchCode) {
+		this.branchCode = branchCode;
+	}
+
+	public Long getPaidAmount() {
+		return paidAmount;
+	}
+
+	public void setPaidAmount(Long paidAmount) {
+		this.paidAmount = paidAmount;
+	}
+
+	public String getSource() {
+		return source;
+	}
+
+	public void setSource(String source) {
+		this.source = source;
+	}
+
+	public String getClearing() {
+		return clearing;
+	}
+
+	public void setClearing(String clearing) {
+		this.clearing = clearing;
+	}
+
+	public Long getRefid() {
+		return refid;
+	}
+
+	public void setRefid(Long refid) {
+		this.refid = refid;
+	}
+
+	public String getAccountNo() {
+		return accountNo;
+	}
+
+	public void setAccountNo(String accountNo) {
+		this.accountNo = accountNo;
+	}
+
+	public String getClearingSap() {
+		return clearingSap;
+	}
+
+	public void setClearingSap(String clearingSap) {
+		this.clearingSap = clearingSap;
+	}
+
+	public Long getPaymentInvoiceManualId() {
+		return paymentInvoiceManualId;
+	}
+
+	public void setPaymentInvoiceManualId(Long paymentInvoiceManualId) {
+		this.paymentInvoiceManualId = paymentInvoiceManualId;
+	}
+
+	public Long getBeforVat() {
+		return beforVat;
+	}
+
+	public void setBeforVat(Long beforVat) {
+		this.beforVat = beforVat;
+	}
+
+	public Long getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Long amount) {
+		this.amount = amount;
+	}
+
+	public int getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(int customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCustomerAddress() {
+		return customerAddress;
+	}
+
+	public void setCustomerAddress(String customerAddress) {
+		this.customerAddress = customerAddress;
+	}
+
+	public String getCustomerSegment() {
+		return customerSegment;
+	}
+
+	public void setCustomerSegment(String customerSegment) {
+		this.customerSegment = customerSegment;
+	}
+
+	public String getCustomerBranch() {
+		return customerBranch;
+	}
+
+	public void setCustomerBranch(String customerBranch) {
+		this.customerBranch = customerBranch;
+	}
+
+	public String getTaxNo() {
+		return taxNo;
+	}
+
+	public void setTaxNo(String taxNo) {
+		this.taxNo = taxNo;
+	}
+
+	public String getSubNo() {
+		return subNo;
+	}
+
+	public void setSubNo(String subNo) {
+		this.subNo = subNo;
+	}
+
+	public String getPeriod() {
+		return period;
+	}
+
+	public void setPeriod(String period) {
+		this.period = period;
+	}
+
+	public String getServiceType() {
+		return serviceType;
+	}
+
+	public void setServiceType(String serviceType) {
+		this.serviceType = serviceType;
+	}
+
+	public String getPrintReceipt() {
+		return printReceipt;
+	}
+
+	public void setPrintReceipt(String printReceipt) {
+		this.printReceipt = printReceipt;
+	}
+
+	public String getRemark() {
+		return remark;
+	}
+
+	public void setRemark(String remark) {
+		this.remark = remark;
+	}
+
+	public String getCreateBy() {
+		return createBy;
+	}
+
+	public void setCreateBy(String createBy) {
+		this.createBy = createBy;
+	}
+
+	public Timestamp getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Timestamp createDate) {
+		this.createDate = createDate;
+	}
+
+	public String getUpdateBy() {
+		return updateBy;
+	}
+
+	public void setUpdateBy(String updateBy) {
+		this.updateBy = updateBy;
+	}
+
+	public Timestamp getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Timestamp updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public String getRecordStatus() {
+		return recordStatus;
+	}
+
+	public void setRecordStatus(String recordStatus) {
+		this.recordStatus = recordStatus;
+	}
+
+}
