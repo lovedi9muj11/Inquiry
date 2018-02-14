8cd79a8f28ce1fa9dd0c7a7ec430d1485289c3f2
Evolution Tea <Evolution Tea@DESKTOP-43087BR>
2018-02-14 17:58:14 +0700

    update controller Histrory Payment

--- src/main/java/th/co/maximus/controller/HistroryPaymentController.java
+++ src/main/java/th/co/maximus/controller/HistroryPaymentController.java
@@ -1,15 +1,41 @@
 package th.co.maximus.controller;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import th.co.maximus.bean.PaymentMMapPaymentInvBean;
+import th.co.maximus.service.HistoryPaymentService;
 
 @Controller
 public class HistroryPaymentController {
+	
+	@Autowired
+	private HistoryPaymentService paymentManualService;
 	
 	  @RequestMapping(value = {"/gotoHistroryPayment"}, method = RequestMethod.GET)
 	    public String gotoHistroryPayment(Model model) {
 	        return "history-payment";
 	    }
+	  
+	  @RequestMapping(value = {"/histroryPayment/find"}, method = RequestMethod.POST, produces = "application/json")
+	  @ResponseBody
+	    public List<PaymentMMapPaymentInvBean> find(@RequestBody PaymentMMapPaymentInvBean creteria) {
+		  List<PaymentMMapPaymentInvBean> result = new ArrayList<>();
+		  if("".equals(creteria.getAccountNo())) {
+			  result = paymentManualService.servicePaymentHitrory();	
+		  }else {
+			  result = paymentManualService.serviceHistroryPaymentFromAccountNo(creteria.getAccountNo());	
+			  
+		  }
+	        return result;
+	    }
+
 }
