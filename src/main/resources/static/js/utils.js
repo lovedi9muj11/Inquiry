023eb18c5bc7c244fedd1003fe3221de7a6ab493
Xunxas <a.e_anusorn@hotmail.com>
2018-02-22 00:42:39 +0700

    format number

--- src/main/resources/static/js/utils.js
+++ src/main/resources/static/js/utils.js
@@ -1,3 +1,10 @@
+$(document).ready(function() {
+	$('.format4D').autoNumeric({vMin:'0.0000',vMax:'99999999999.9999'});
+	$('.format2D').autoNumeric({vMin:'0.00',vMax:'9999999999999.99'});
+	$('.numeric2point').autoNumeric({vMin:'0.00',vMax:'9999999999.99'});
+	$('.format2DSib').autoNumeric({vMin:'0.00',vMax:'99.99'});
+});
+
 function formatDouble(num, scale) {
 	if(scale === undefined) scale = 2;
 	if(!$.isNumeric(num)) return num;
