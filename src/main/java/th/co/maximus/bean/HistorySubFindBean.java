7f65325dc64853c3695055e2da80009862741d98
Xunxas <a.e_anusorn@hotmail.com>
2018-02-24 14:12:05 +0700

    creteria

---
+++ src/main/java/th/co/maximus/bean/HistorySubFindBean.java
@@ -0,0 +1,56 @@
+package th.co.maximus.bean;
+
+public class HistorySubFindBean {
+	
+	private String payDate;
+	private String payDateTo;
+	private String accountCode;
+	private String vatRate;
+	private String pos;
+	private String user;
+	private String payType;
+	
+	public String getPayDate() {
+		return payDate;
+	}
+	public void setPayDate(String payDate) {
+		this.payDate = payDate;
+	}
+	public String getPayDateTo() {
+		return payDateTo;
+	}
+	public void setPayDateTo(String payDateTo) {
+		this.payDateTo = payDateTo;
+	}
+	public String getAccountCode() {
+		return accountCode;
+	}
+	public void setAccountCode(String accountCode) {
+		this.accountCode = accountCode;
+	}
+	public String getVatRate() {
+		return vatRate;
+	}
+	public void setVatRate(String vatRate) {
+		this.vatRate = vatRate;
+	}
+	public String getPos() {
+		return pos;
+	}
+	public void setPos(String pos) {
+		this.pos = pos;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPayType() {
+		return payType;
+	}
+	public void setPayType(String payType) {
+		this.payType = payType;
+	}
+
+}
