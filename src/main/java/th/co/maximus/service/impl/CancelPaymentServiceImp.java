c7ba917cab8261322f59308521c7df5afa1f2711
DESKTOP-43087BR\Evolution Tea <evolutioncomsci@gmail.com>
2018-04-30 15:30:47 +0700

    update insert and update status cancel payment service and update service show data table in page cancel payment

--- src/main/java/th/co/maximus/service/impl/CancelPaymentServiceImp.java
+++ src/main/java/th/co/maximus/service/impl/CancelPaymentServiceImp.java
@@ -51,42 +51,27 @@ public class CancelPaymentServiceImp implements CancelPaymentService {
 
 	@Override
 	public List<PaymentMMapPaymentInvBean> findAllCancelPayment() {
-		List<PaymentMMapPaymentInvBean> result = new ArrayList<PaymentMMapPaymentInvBean>();
-		for(PaymentMMapPaymentInvBean paymentMMapPaymentInvBean :paymentInvoiceManualDao.findPaymentMuMapPaymentInV()) {
-			paymentMMapPaymentInvBean.setPaidDateStr(dt.format(paymentMMapPaymentInvBean.getPaidDate()));
-			paymentMMapPaymentInvBean.setCreateDateStr(dt.format(paymentMMapPaymentInvBean.getCreateDate()));
-			result.add(paymentMMapPaymentInvBean);
-		}
-		return result;
+		return paymentInvoiceManualDao.findPaymentMuMapPaymentInV();
 	}
 
 	@Override
 	public List<PaymentMMapPaymentInvBean> findAllCancelPaymentFromId(long manualId) {
-		List<PaymentMMapPaymentInvBean> result = new ArrayList<PaymentMMapPaymentInvBean>();
-		for(PaymentMMapPaymentInvBean paymentMMapPaymentInvBean :paymentInvoiceManualDao.findPaymentMuMapPaymentInVFromId(manualId)) {
-			paymentMMapPaymentInvBean.setPaidDateStr(dt.format(paymentMMapPaymentInvBean.getPaidDate()));
-			paymentMMapPaymentInvBean.setCreateDateStr(dt.format(paymentMMapPaymentInvBean.getCreateDate()));
-			result.add(paymentMMapPaymentInvBean);
-		}
-		return result;
+		return paymentInvoiceManualDao.findPaymentMuMapPaymentInVFromId(manualId);
 	}
 
 	@Override
 	public List<PaymentMMapPaymentInvBean> serviceCriteriaFromInvoiceOrReceiptNo(String receiptNo, String invoiceNo) {
-		List<PaymentMMapPaymentInvBean> result = new ArrayList<PaymentMMapPaymentInvBean>();
-		for(PaymentMMapPaymentInvBean paymentMMapPaymentInvBean :paymentInvoiceManualDao.findCriteriaFromInvoiceOrReceiptNo(receiptNo, invoiceNo)) {
-			paymentMMapPaymentInvBean.setPaidDateStr(dt.format(paymentMMapPaymentInvBean.getPaidDate()));
-			paymentMMapPaymentInvBean.setCreateDateStr(dt.format(paymentMMapPaymentInvBean.getCreateDate()));
-			result.add(paymentMMapPaymentInvBean);
-		}
-		return result;
+		return paymentInvoiceManualDao.findCriteriaFromInvoiceOrReceiptNo(receiptNo, invoiceNo);
 	}
 
 	@Override
 	public boolean insertAndUpdateCancelPayment(PaymentMMapPaymentInvBean paymentMMapPaymentInvBean) {
 		boolean resultReturn = true;
 		PaymentInvoiceManualBean paymentInvoiceManualBean = new PaymentInvoiceManualBean();
+		int manualID = 0;
 		try {
+			
+			//update status
 			paymentInvoiceManualDao.updateRecodeStatusFromReceiptNo("C", paymentMMapPaymentInvBean.getManualId());
 			paymentInvoiceManualDao.updateStatusPaymentInvoice(paymentMMapPaymentInvBean.getManualId());
 			if ("02".equals(paymentMMapPaymentInvBean.getStatusCancelPayment())) {
@@ -107,18 +92,18 @@ public class CancelPaymentServiceImp implements CancelPaymentService {
 						paymentManualBean.setCreateBy(resultPaymentManual.getCreateBy());
 						paymentManualBean.setCreateDate(resultPaymentManual.getCreateDate());
 						paymentManualBean.setUpdateBy(resultPaymentManual.getUpdateBy());
-						paymentManualBean.setUpdateDate(resultPaymentManual.getUpdateDate());
+						//paymentManualBean.setUpdateDate(resultPaymentManual.getUpdateDate());
 						paymentManualBean.setRecordStatus("A");
 						paymentManualBean.setAccountNo(resultPaymentManual.getAccountNo());
 						paymentManualBean.setPaytype(resultPaymentManual.getPaytype());
 						paymentManualBean.setDocType(resultPaymentManual.getDocType());
-						paymentManualDao.insertPayment(paymentManualBean);
+						manualID =  paymentManualDao.insertPayment(paymentManualBean);
 					}
 				}
 				//Insert PaymentInvoice
 				List<PaymentInvoiceManualBean> resultPaymentInvoice = paymentInvoiceManualDao.findPaymentInvoiceFromManualId(paymentMMapPaymentInvBean.getManualId());
 				if(!resultPaymentInvoice.isEmpty()) {
-					paymentInvoiceManualBean.setManualId(resultPaymentInvoice.get(0).getManualId());
+					paymentInvoiceManualBean.setManualId(Long.valueOf(manualID));
 					paymentInvoiceManualBean.setSource(resultPaymentInvoice.get(0).getSource());
 					paymentInvoiceManualBean.setInvoiceNo(resultPaymentInvoice.get(0).getInvoiceNo());
 					paymentInvoiceManualBean.setBeforVat(resultPaymentInvoice.get(0).getBeforVat());
@@ -145,105 +130,8 @@ public class CancelPaymentServiceImp implements CancelPaymentService {
 					paymentInvoiceManualBean.setDiscountspecial(resultPaymentInvoice.get(0).getDiscountspecial());
 					paymentInvoiceManualBean.setAmounttype(resultPaymentInvoice.get(0).getAmounttype());
 					paymentInvoiceManualBean.setDepartment(resultPaymentInvoice.get(0).getDepartment());
-
 					paymentInvoiceManualDao.insert(paymentInvoiceManualBean);
-				}
-				//insert TrsChequeRefManual
-				List<TrsChequeRefManualBean> trsChequeRefManual = trsChequeRefManualDao.findTrachequeFromManualId(paymentMMapPaymentInvBean.getManualId());
-				if(!trsChequeRefManual.isEmpty()) {
-					for(TrsChequeRefManualBean resultTrsChe : trsChequeRefManual) {
-						TrsChequeRefManualBean trsChequeRefManualBean = new TrsChequeRefManualBean();
-						trsChequeRefManualBean.setChequeNo(resultTrsChe.getChequeNo());
-						trsChequeRefManualBean.setPublisherId(resultTrsChe.getPublisherId());
-						trsChequeRefManualBean.setPublisher(resultTrsChe.getPublisher());
-						trsChequeRefManualBean.setBranch(resultTrsChe.getBranch());
-						trsChequeRefManualBean.setaMount(resultTrsChe.getaMount());
-						trsChequeRefManualBean.setUpdateDttm(resultTrsChe.getUpdateDttm());
-						trsChequeRefManualBean.setUpdateSystem(resultTrsChe.getUpdateSystem());
-						trsChequeRefManualBean.setUpdateUser(resultTrsChe.getUpdateUser());
-						trsChequeRefManualBean.setVersionStamp(resultTrsChe.getVersionStamp());
-						trsChequeRefManualBean.setChequeDate(resultTrsChe.getChequeDate());
-						trsChequeRefManualBean.setBounceChequeDate(resultTrsChe.getBounceChequeDate());
-						trsChequeRefManualBean.setReverseArDate(resultTrsChe.getReverseArDate());
-						trsChequeRefManualBean.setBounceStatus(resultTrsChe.getBounceStatus());
-						trsChequeRefManualBean.setMethodManualId(resultTrsChe.getMethodManualId());
-						trsChequeRefManualDao.insert(trsChequeRefManualBean);
-					}
-				}
-				//insert TrscreDitrefManual
-				List<TrscreDitrefManualBean> resultTrscreDitrefManual = trscreDitrefManualDao.trscreDitrefManualFromManualId(paymentMMapPaymentInvBean.getManualId());
-				if(!resultPaymentInvoice.isEmpty()) {
-					for(TrscreDitrefManualBean result : resultTrscreDitrefManual) {
-						TrscreDitrefManualBean trscreDitrefManualBean = new TrscreDitrefManualBean();
-						trscreDitrefManualBean.setCreditNo(result.getCreditNo());
-						trscreDitrefManualBean.setPublisherdec(result.getPublisherdec());
-						trscreDitrefManualBean.setCardType(result.getCardType());
-						trscreDitrefManualBean.setaMount(result.getaMount());
-						trscreDitrefManualBean.setUpdateDttm(result.getUpdateDttm());
-						trscreDitrefManualBean.setUpdateSystem(result.getUpdateSystem());
-						trscreDitrefManualBean.setUpdateUser(result.getUpdateUser());
-						trscreDitrefManualBean.setVersionStamp(result.getVersionStamp());
-						trscreDitrefManualBean.setMethodManualId(result.getMethodManualId());
-						trscreDitrefManualDao.insertTrscreDitrefManua(trscreDitrefManualBean);
-					}
-					
-				}
-				//insert trsMethodManual
-				List<TrsMethodManualBean> resultTrsMethodManual = trsMethodManualDao.findTrsMethodManualFromManualId(paymentMMapPaymentInvBean.getManualId());
-				if(!resultTrsMethodManual.isEmpty()) {
-					for(TrsMethodManualBean result : resultTrsMethodManual) {
-						TrsMethodManualBean trsMethodManualBean = new TrsMethodManualBean();
-						trsMethodManualBean.setCode(result.getCode());
-						trsMethodManualBean.setName(result.getName());
-						trsMethodManualBean.setChequeNo(result.getChequeNo());
-						trsMethodManualBean.setAccountNo(result.getAccountNo());
-						trsMethodManualBean.setAmount(result.getAmount());
-						trsMethodManualBean.setUpdateDttm(result.getUpdateDttm());
-						trsMethodManualBean.setUpdateSystem(result.getUpdateSystem());
-						trsMethodManualBean.setUpdateUser(result.getUpdateUser());
-						trsMethodManualBean.setVersionStamp(result.getVersionStamp());
-						trsMethodManualBean.setOffsetDocumentNo(result.getOffsetDocumentNo());
-						trsMethodManualBean.setOffsetAccountCode(result.getOffsetAccountCode());
-						trsMethodManualBean.setOffsetAccountName(result.getOffsetAccountName());
-						trsMethodManualBean.setRemark(result.getRemark());
-						trsMethodManualBean.setCreateBy(result.getCreateBy());
-						trsMethodManualBean.setCreateDate(result.getCreateDate());
-						trsMethodManualBean.setUpdateBy(result.getUpdateBy());
-						trsMethodManualBean.setUpdateDate(result.getUpdateDate());
-						trsMethodManualBean.setRecordStatus(result.getRecordStatus());
-						trsMethodManualBean.setRefId(result.getRefId());
-						trsMethodManualBean.setDeductionManualId(result.getDeductionManualId());
-						trsMethodManualBean.setManualId(result.getManualId());
-						
-						trsMethodManualDao.insertTrsMethod(trsMethodManualBean);
-					}
-					
-					//insert deduction manual
-					List<DeductionManualBean> resultDeductionManual = deductionManualDao.findDeductionManualFromManualId(paymentMMapPaymentInvBean.getManualId());
-					if(!resultDeductionManual.isEmpty()) {
-						for(DeductionManualBean result : resultDeductionManual) {
-							DeductionManualBean deductionManualBean = new DeductionManualBean();
-							deductionManualBean.setDeDuctionNo(result.getDeDuctionNo());
-							deductionManualBean.setDeDuctionType(result.getDeDuctionType());
-							deductionManualBean.setaMount(result.getaMount());
-							deductionManualBean.setPaymentDate(result.getPaymentDate());
-							deductionManualBean.setUpdateDttm(result.getUpdateDttm());
-							deductionManualBean.setUpdateSystem(result.getUpdateSystem());
-							deductionManualBean.setUpdateUser(result.getUpdateUser());
-							deductionManualBean.setVersionStamp(result.getVersionStamp());
-							deductionManualBean.setInvoiceNo(result.getInvoiceNo());
-							deductionManualBean.setRemark(result.getRemark());
-							deductionManualBean.setCreateBy(result.getCreateBy());
-							deductionManualBean.setCreateDate(result.getCreateDate());
-							deductionManualBean.setUpdateBy(result.getUpdateBy());
-							deductionManualBean.setUpdateDate(result.getUpdateDate());
-							deductionManualBean.setRecordStatus(result.getRecordStatus());
-							deductionManualBean.setManualId(result.getManualId());
-							
-							deductionManualDao.insert(deductionManualBean);
-						}
-					}
-				}
+				}		
 			}
 		}catch (Exception e) {
 			resultReturn = false;
