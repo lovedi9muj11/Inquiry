527a1c5a0e51e83e261fe6dbbdf2e804d7f3d46c
DESKTOP-20LHSP8\maax <max.musquito@gmail.com>
2018-07-12 22:25:38 +0700

    no message

--- src/main/java/th/co/maximus/auth/controller/UserAuthController.java
+++ src/main/java/th/co/maximus/auth/controller/UserAuthController.java
@@ -3,13 +3,17 @@ package th.co.maximus.auth.controller;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import th.co.maximus.auth.model.UserDto;
 import th.co.maximus.auth.service.SecurityService;
 import th.co.maximus.auth.service.UserService;
@@ -48,15 +52,19 @@ public class UserAuthController {
     }
 
     @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login(Model model, String error, String logout) {
+    public String login(Model model, String error, String logout,HttpServletRequest request, HttpServletResponse response) {
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         if (error != null)
             model.addAttribute("error", "Your username and password is invalid.");
 
         if (logout != null)
             model.addAttribute("message", "You have been logged out successfully.");
+            SecurityContextLogoutHandler securityContextLogoutHandler = new SecurityContextLogoutHandler();
+			securityContextLogoutHandler.logout(request, response, auth);
 
         return "login";
     }
+
     
     @RequestMapping(value = {"/", "/welcome"}, method = RequestMethod.GET)
     public String welcome(Model model) {
--- src/main/java/th/co/maximus/auth/service/UserDetailsServiceImpl.java
+++ src/main/java/th/co/maximus/auth/service/UserDetailsServiceImpl.java
@@ -2,11 +2,14 @@ package th.co.maximus.auth.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,14 +33,19 @@ public class UserDetailsServiceImpl implements UserDetailsService{
     @Transactional(readOnly = true)
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         UserDto user = userRepository.findByUsername(username);
-        
-        List<GrantedAuthority> authorities = new ArrayList<>();
-        for (Role role : user.getRoles()){
-        	authorities.add(new SimpleGrantedAuthority(role.getName()));
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if(auth == null){
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            for (Role role : user.getRoles()){
+                authorities.add(new SimpleGrantedAuthority(role.getName()));
+            }
+            UserProfile myUserDetails = new UserProfile(user.getUsername(), user.getPassword(), authorities);
+            myUserDetails.setPos(posNo);
+            myUserDetails.setRoles(user.getRoles());
+            return myUserDetails;
+        }else{
+          return null;
         }
-        UserProfile myUserDetails = new UserProfile(user.getUsername(), user.getPassword(), authorities);
-        myUserDetails.setPos(posNo);
-        myUserDetails.setRoles(user.getRoles());
-        return myUserDetails;
+       
     }
 }
