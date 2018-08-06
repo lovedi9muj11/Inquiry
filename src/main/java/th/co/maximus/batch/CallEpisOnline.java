60651ad1cdb14202459764eee5c05e80738a3c85
DESKTOP-20LHSP8\maax <max.musquito@gmail.com>
2018-08-06 20:38:20 +0700

    no message

--- src/main/java/th/co/maximus/batch/CallEpisOnline.java
+++ src/main/java/th/co/maximus/batch/CallEpisOnline.java
@@ -1,5 +1,8 @@
 package th.co.maximus.batch;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,49 +13,54 @@ import th.co.maximus.service.CallEpisOnlineService;
 
 @RestController
 public class CallEpisOnline {
-	
+
 	@Autowired
 	CallEpisOnlineService callEpisOnlineService;
-	
-	public void callOnline(){
+
+	public void callOnline() {
 		try {
 			callEpisOnlineService.callOnline();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	@RequestMapping(value = "/syncMasterData", method = RequestMethod.GET, produces = "application/json")
-	public String callOnlineSyncMasterData(){
+	public Map<String, String> callOnlineSyncMasterData() {
 		try {
 			callEpisOnlineService.callOnlineSyncMasterData();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	return Constants.SUCCESS;
-		
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("result", "SUCCESS");
+		return result;
+
 	}
-	
+
 	@RequestMapping(value = "/syncMapGL", method = RequestMethod.GET, produces = "application/json")
-	public String callOnlineSyncMapGL(){
+	public Map<String, String> callOnlineSyncMapGL() {
 		try {
 			callEpisOnlineService.callOnlineSyncMapGL();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	return Constants.SUCCESS;
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("result", "SUCCESS");
+		return result;
 	}
-	
+
 	@RequestMapping(value = "/syncUser", method = RequestMethod.GET, produces = "application/json")
-	public String callOnlineSyncUser(){
+	public Map<String, String> callOnlineSyncUser() {
 		try {
 			callEpisOnlineService.callOnlineSyncUser();
-		}catch (Exception e) {
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-	return Constants.SUCCESS;
+		Map<String, String> result = new HashMap<String, String>();
+		result.put("result", "SUCCESS");
+		return result;
 	}
-	
-}
 
+}
