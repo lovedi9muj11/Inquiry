3347be61b1ef86dbfa05ea2dd18d45f2f0cf7c4a
Chickenz <a.e_anusorn@hotmail.com>
2020-03-27 00:29:09 +0700

    conflict

--- src/main/java/th/co/maximus/dao/MapGLDaoImp.java
+++ src/main/java/th/co/maximus/dao/MapGLDaoImp.java
@@ -70,10 +70,28 @@ public class MapGLDaoImp implements MapGLDao{
 	}
 	
 	@Override
+	public List<MapGLBean> findByProductSource(String revennuId) {
+		StringBuilder sql = new StringBuilder();
+		sql.append(" SELECT * FROM MAP_GL_SERVICE_TYPE mg  ");
+		sql.append(" WHERE mg.PRODUCT_CODE = '"+revennuId+"' ");
+		sql.append(" AND mg.SOURCE = '"+Constants.MasterData.OTHER+"' ");
+		return jdbcTemplate.query(sql.toString() , new mapGL());
+	}
+	
+	@Override
 	public List<MapGLBean> findBySourceOther() {
 		StringBuilder sql = new StringBuilder();
 		sql.append(" SELECT * FROM MAP_GL_SERVICE_TYPE mg  ");
 		sql.append(" WHERE mg.SOURCE = '"+Constants.MasterData.OTHER+"' AND mg.STATUS = '1' ");
+		return jdbcTemplate.query(sql.toString() , new mapGL());
+	}
+
+	@Override
+	public List<MapGLBean> findBySourceOtherProdCode(String prodCode) {
+		StringBuilder sql = new StringBuilder();
+		sql.append(" SELECT * FROM MAP_GL_SERVICE_TYPE mg  ");
+		sql.append(" WHERE mg.SOURCE = '"+Constants.MasterData.OTHER+"' AND mg.STATUS = '1' ");
+		sql.append(" AND mg.PRODUCT_CODE =  '"+prodCode+"'");
 		return jdbcTemplate.query(sql.toString() , new mapGL());
 	}
 
