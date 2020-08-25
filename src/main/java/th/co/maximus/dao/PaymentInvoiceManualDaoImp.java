2506793725cf0ab11ef20c86e7a26c8417d13015
MAx <max.musquito@gmail.com>
2020-08-25 10:55:08 +0700

    joe update

--- src/main/java/th/co/maximus/dao/PaymentInvoiceManualDaoImp.java
+++ src/main/java/th/co/maximus/dao/PaymentInvoiceManualDaoImp.java
@@ -495,8 +495,9 @@ public class PaymentInvoiceManualDaoImp implements PaymentInvoiceManualDao {
 			StringBuilder sqlStmt = new StringBuilder();
 			sqlStmt.append(
 					"SELECT pim.INVOICE_NO ,pim.BEFOR_VAT,pim.VAT_AMOUNT,pim.AMOUNT,(CASE WHEN pim.VAT_RATE = 'Non-VAT' THEN null ELSE pim.VAT_RATE END) AS  VAT_RATE ,pim.CUSTOMER_NAME,pim.CUSTOMER_ADDRESS,pim.CUSTOMER_SEGMENT,pim.CUSTOMER_BRANCH,pim.TAXNO,pim.ACCOUNTSUBNO,pim.PERIOD,pim.SERVICE_TYPE,pim.REMARK,pim.QUANTITY,pim.INCOMETYPE,pim.DISCOUNTBEFORVAT,pim.DISCOUNTSPECIAL,pim.AMOUNTTYPE,pim.DEPARTMENT,pim.SERVICENAME,pim.SERVICECODE,pim.INVOICE_DATE,pim.INCOMEUNIT , pim.DEPARTMENT_AREA ");
-			sqlStmt.append(" ,pim.SEGMENT_CODE , pim.PRODUCT_CODE ");
+			sqlStmt.append(" ,pim.SEGMENT_CODE , pim.PRODUCT_CODE , A.VALUE ");
 			sqlStmt.append(" FROM PAYMENT_INVOICE_MANUAL pim ");
+			sqlStmt.append(" LEFT JOIN MASTER_DATA A ON A.PROPERTY_1 =  pim.DEPARTMENT_AREA AND A.PROPERTY_2 = pim.DEPARTMENTCODE ");
 			sqlStmt.append(" WHERE  pim.MANUAL_ID = ?  ");
 
 			sqlStmt.append(" ORDER BY pim.CREATE_DATE DESC");
@@ -663,7 +664,7 @@ public class PaymentInvoiceManualDaoImp implements PaymentInvoiceManualDao {
 			paymentInvoiceEpisOffline.setServiceCode(rs.getString("pim.SERVICECODE"));
 			paymentInvoiceEpisOffline.setInvoiceDate(rs.getDate("pim.INVOICE_DATE"));
 			paymentInvoiceEpisOffline.setIncomeUnit(rs.getString("pim.INCOMEUNIT"));
-			paymentInvoiceEpisOffline.setDepartmentArea(rs.getString("pim.DEPARTMENT_AREA"));
+			paymentInvoiceEpisOffline.setDepartmentArea(rs.getString("A.VALUE"));
 			paymentInvoiceEpisOffline.setSegmentCode(rs.getString("pim.SEGMENT_CODE"));
 			paymentInvoiceEpisOffline.setProductCode(rs.getString("pim.PRODUCT_CODE"));
 			
